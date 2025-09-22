7f33ba93f85282c3e71007634fa78916477e6d30
henriquegpaiva <goncalves.paiva@escola.pr.gov.br>
2025-09-22 08:07:03 -0300

    Create exer10.js

---
+++ exer10.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 10;
+
+// Operadores aritiméticos
+console.log("Soma:", a + b);
+console.log("Subtração:", a - b);
+console.log("Multiplicação:", a * b);
+console.log("Divisão:", a / b);
+console.log("Resto:", a % b);
+
+// Operadores relacionais
+console.log("a é maior que b?", a < b);
+console.log("a é menor que b?", a > b);
+console.log("a é identico que b?", a === b);
+
+// Operadores lógicos AND e OR
+let condicao = (a > 0 && b > 0) || (a < 0);
+console.log("Condição lógica:", condicao);
