809c223cab7e883bc7ab95ab5c83d30824ce1c30
henriquegpaiva <goncalves.paiva@escola.pr.gov.br>
2025-09-22 07:56:09 -0300

    Create exer2.js

---
+++ exer2.js
@@ -0,0 +1,10 @@
+// 1. Declare as variáveis
+const nome  = 'Maria';
+const idade = 30;
+const cidade = 'Rio de Janeiro';
+
+// 2. Use as variáveis para criar uma frase com Template Literal
+const fraseCompleta = `Olá, meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`;
+
+// 3. Imprima a frase no terminal
+console.log(fraseCompleta);
