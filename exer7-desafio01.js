b54366054a1346768cb4822b89a9e1393c88afd8
henriquegpaiva <goncalves.paiva@escola.pr.gov.br>
2025-09-22 07:59:51 -0300

    Create exer7-desafio01.js

---
+++ exer7-desafio01.js
@@ -0,0 +1,14 @@
+// ####################                    #################### //
+// ####################      Desafio 01    #################### //
+// ####################                    #################### //
+
+// FAÇA A MÉDIA DE QUATRO IDADES
+
+const idades = {
+    emanoel: 15,
+    leonardo: 17,
+    felipe: 16,
+    henrique: 15
+}
+
+const media = (idades.emanoel + idades.leonardo + idades.felipe + idades.henrique) / 4
